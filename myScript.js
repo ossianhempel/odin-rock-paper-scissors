5a1badc4c3ff9c251aaf55097a93c654a4bf9488
ossianhempel <hemposse@hotmail.com>
2022-08-13 13:45:34 +0200

    Create getComputerChoice() which randomly return rock, paper, or scissors

---
+++ myScript.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+    const choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+console.log(getComputerChoice())
+
+
+function game() {
+    for (let i = 0; i < 5; i++){
+
+    }
+}
